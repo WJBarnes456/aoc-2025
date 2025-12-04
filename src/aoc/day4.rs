a24f6b557deeea1463348267d68d565af7d010c7
Will Barnes <will@wjbarn.es>
2025-12-04 20:01:09 +0000

    [day4] Implement part 2
    Nice easy one today. Would've been nice to have a sufficiently massive grid to
    need a cleverer solution but this is more than fine for now.

--- src/aoc/day4.rs
+++ src/aoc/day4.rs
@@ -26,7 +26,7 @@ fn surrounding_rolls(grid: &Vec<Vec<bool>>, x: isize, y: isize) -> usize {
             }
 
             // and don't consider the square we're currently looking at
-            if (x_offset == 0 && y_offset == 0) {
+            if x_offset == 0 && y_offset == 0 {
                 continue;
             }
 
@@ -48,7 +48,7 @@ impl Puzzle for Day4 {
         for y in 0..grid.len() {
             for x in 0..grid[y].len() {
                 if grid[y][x] && surrounding_rolls(&grid, x as isize, y as isize) < 4 {
-                    println!("accessible roll at ({x},{y})");
+                    //println!("accessible roll at ({x},{y})");
                     accessible += 1;
                 } 
             }
@@ -56,7 +56,28 @@ impl Puzzle for Day4 {
         return accessible.to_string();
     }
     fn part2(&self, input: &String) -> String {
-        return input.chars().rev().collect::<String>();
+        // the tricky thing here is that we might be able to remove from e.g. the last row, so we'll have to continue going until we did a pass without removing any
+        // I was lazy and chose to reuse my part 1 code, but a more efficient way of solving this would be to map the entire grid to counts, and propagate to neighbouring nodes
+        // i.e. structure it as a graph problem where you visit each node at most 4 times (once 4 of its 8 neighbours have changed) which makes it linear in the size of the input
+        // still, this is more than fast enough so no need to overthink it
+        let mut grid = to_grid(input);
+        let mut changes = true;
+        let mut accessible = 0;
+        while changes {
+            changes = false;
+            for y in 0..grid.len() {
+                for x in 0..grid[y].len() {
+                    if grid[y][x] && surrounding_rolls(&grid, x as isize, y as isize) < 4 {
+                        //println!("removing accessible roll at ({x},{y})");
+                        accessible += 1;
+                        grid[y][x] = false;
+                        changes = true;
+                    }
+                }
+            }
+        }
+
+        return accessible.to_string();
     }
 }
 
