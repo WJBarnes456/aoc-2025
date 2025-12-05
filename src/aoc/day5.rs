5754919475e6f60fda7bb0eb19de1d77b2acbfc6
Will Barnes <will@wjbarn.es>
2025-12-05 21:13:07 +0000

    [day5] Implement (buggy) part 2
    I'm not checking ranges properly, since 9-9 1-10 doesn't pick up an overlap.
    Storing the current state so I can roll back if I messed up

--- src/aoc/day5.rs
+++ src/aoc/day5.rs
@@ -1,4 +1,6 @@
 use crate::Puzzle;
+use std::cmp::min;
+use std::cmp::max;
 // Day5 implements day 5 of AoC 2025, as uploaded at https://adventofcode.com/2025/day/5. 
 pub struct Day5;
 
@@ -22,6 +24,38 @@ fn process_input(input: &String) -> (Vec<usize>, Vec<usize>, Vec<usize>) {
     return (starts, ends, available);
 }
 
+// add_to_ranges adds a new range to the ranges that already exist
+// we maintain this list in sorted order since adding a new range might cause 2 other ranges to merge together
+// e.g. consider we have ranges 1-2 and 4-5. if we insert range 3-3 this becomes a single range 1-5
+// I checked my input playing around in Python and the input ranges _are_ overlapping so we have to do this
+fn add_to_ranges(ranges: &mut Vec<(usize, usize)>, new_start_p: &usize, new_end_p: &usize) {
+    let (new_start, new_end) = (*new_start_p, *new_end_p);
+    let mut i = 0;
+    while i < ranges.len() {
+        let (existing_start, existing_end) = ranges[i];
+        // cases
+        // no overlap, too early - new_end < existing_start - when we encounter this for the first time, this is a new value! so we need to push it into the array at this point
+        if (new_end < existing_start) {
+            ranges.insert(i, (new_start, new_end));
+            return;
+        }
+
+        // some overlap - existing_start <= new_end <= existing_end or existing_start <= new_start <= existing_end.
+        // in this case, we have a new range we want to insert - it's inefficient, but correct, to remove the old range, and try and insert this new one instead
+        if ((existing_start <= new_start) && (new_start <= existing_end)) || ((existing_start <= new_end) && (new_end <= existing_end)) {
+            ranges.remove(i);
+            add_to_ranges(ranges, &min(existing_start, new_start), &max(existing_end, new_end));
+            return;
+        }
+
+        i+=1;
+    }
+
+    // no overlap, too late
+    // we iterated over the entire array and didn't have any overlap, so this is a new value at the very end
+    ranges.push((new_start, new_end));
+}
+
 impl Puzzle for Day5 {
     fn part1(&self, input: &String) -> String {
         let (starts, ends, available) = process_input(input);
@@ -37,7 +71,20 @@ impl Puzzle for Day5 {
         return count.to_string();
     }
     fn part2(&self, input: &String) -> String {
-        let (start, _) = input.split_at(10); 
-        return start.to_string();
+        let (starts, ends, _) = process_input(input);
+
+        let mut ranges: Vec<(usize, usize)> = Vec::new();
+
+        let range_count = starts.iter().zip(ends.iter())
+            .map(|(start, end)| add_to_ranges(&mut ranges, start, end))
+            .count();
+
+        println!("added {range_count} ranges to ranges");
+
+        for (start, end) in ranges.iter() {
+            println!("range: ({start}, {end}), size {}", end - start + 1);
+        }
+
+        return ranges.iter().map(|(start, end)| end - start + 1).sum::<usize>().to_string();
     }
 }
