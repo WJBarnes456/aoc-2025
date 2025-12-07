4fe3917e4049438d1726eeaef7248a9330376e00
Will Barnes <will@wjbarn.es>
2025-12-07 12:30:18 +0000

    [day7] Implement part 1
    The splitter index stuff where we translate lazily rather than eagerly looks
    kind of nasty but I'm really happy with it, it feels like writing functional
    code again.
    
    Part 2 looks like it should be a relatively straightforward update to using a
    hashmap and tracking the number of different ways to get to each position rather
    than just the set.

---
+++ src/aoc/day7.rs
@@ -0,0 +1,47 @@
+use crate::Puzzle;
+
+use std::collections::HashSet;
+
+// Day7 implements day 7 of AoC 2025, as uploaded at https://adventofcode.com/2025/day/7. 
+pub struct Day7;
+
+// important info:
+// starts at S, splits at ^, passes through .
+// beams always move downward (i.e. we don't need to model every single location, just where there are splitters)
+// beams are not duplicated - positions must be tracked distinctly
+// what data structure - we really want a sparse lookup of row number to splitter positions
+// we could just as easily do a dense one instead, and honestly since we can iterate over the rows it's not too bad at all
+// then we just need a set to maintain the... well, set of different beam positions
+
+// part 1 - how many times will it be split - i.e. the number of times we have a beam hit a splitter
+
+impl Puzzle for Day7 {
+    fn part1(&self, input: &String) -> String {
+        let mut lines = input.lines();
+        let first_pos = lines.next().unwrap().chars().position(|x| x == 'S').unwrap();
+        let mut positions = HashSet::<usize>::new();
+        positions.insert(first_pos);
+
+        let mut total_splits = 0;
+        for (y, line) in lines.enumerate() {
+            let splitter_indices = line.chars().enumerate().filter(|(_, c)| *c == '^').map(|(i, _)| i);
+            for i in splitter_indices {
+                // if there is a beam at the specified position, then remove it and add the new positions
+                if positions.remove(&i) {
+                    // NB: we might need to handle hitting the edge of the map in the general case but neither the test nor my puzzle input include that 
+                    positions.insert(i-1);
+                    positions.insert(i+1);
+                    total_splits += 1;
+                    println!("beam split at position {i}");
+                }
+            }
+
+            println!("beam positions at y={y}: {positions:?}");
+        }
+
+        return total_splits.to_string();
+    }
+    fn part2(&self, input: &String) -> String {
+        return input.chars().take(10).collect::<String>();
+    }
+}
