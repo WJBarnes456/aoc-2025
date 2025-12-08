f27d6af918c5bc974440f39ec5bb7e378975feab
Will Barnes <will@wjbarn.es>
2025-12-08 23:31:39 +0000

    [day8] Implement more of part 1
    I'm battling the borrow checker here trying to get nth_closest working. I don't want to lose this intermediate state - now I've realised I need to pass tree by reference I'm going to go back to using an iterator to build distances.

--- src/aoc/day8.rs
+++ src/aoc/day8.rs
@@ -85,19 +85,31 @@ struct PointDist {
     dist: usize, // actually the squared distance but this is way easier to type
 }
 
-fn initialise_circuits(length: usize) -> Vec::<Rc<HashSet<usize>>> {
-    let mut circuits = Vec::<Rc<HashSet<usize>>>::new();
+fn initialise_circuits(length: usize) -> Vec::<HashSet<usize>> {
+    let mut circuits = Vec::<HashSet<usize>>::new();
     
     for i in 0..length {
         let mut set = HashSet::<usize>::new();
         set.insert(i);
-        circuits.push(Rc::new(set));
+        circuits.push(set);
     }
 
     return circuits
 }
 
-fn part1(tree: KdTree<Point>) {
+// n is 0-indexed, i.e. 0 returns the 
+// e.g. n=0 returns the second closest point in the tree (since the closest one is always that same point)
+fn nth_closest(tree: &KdTree<Point>, point: &Point, n: usize) -> PointDist {
+    let nearest = tree.nearests(point, n + 2)[n+1];
+    return PointDist{
+        i_point_a: point.i,
+        i_point_b: nearest.item.i,
+        dist: nearest.squared_distance,
+    }
+}
+
+fn part1(tree: KdTree<Point>) -> usize {
+    let points = tree.iter().map(|x| *x).collect::<Vec<Point>>(); 
     // unfortunately the library I'm using doesn't support deleting points from a tree once it's already constructed
     // however we can use a cheeky workaround which isn't the most efficient, but is more than good enough
     //
@@ -105,22 +117,20 @@ fn part1(tree: KdTree<Point>) {
     // i.e. we need two vecs, one of the current index of the nth neighbour, and one with the current nth neighbour point
     // then we also need to keep track of the circuits - easiest way of doing this is going to be having a map of points to the circuits they belong to as a pointer to a set
     // nb. indices must start at 1 because every point's closest point is otherwise itself. I don't think this even assumes there are no duplicates.
-    let mut indices = tree.iter().map(|_| 1_usize).collect::<Vec<usize>>();
-    let mut distances = tree.iter().enumerate().map(| (a_index, point)| {
-            let nearest = tree.nearests(point, 2)[1];
-            return PointDist{
-                i_point_a: a_index,
-                i_point_b: point.i,
-                dist: nearest.squared_distance,
-            }
-        } ).collect::<Vec<PointDist>>();
-    let circuits = initialise_circuits(tree.len());
-    let circuit_map = indices.clone();
+    let mut indices = tree.iter().map(|_| 0_usize).collect::<Vec<usize>>();
+    let mut distances = Vec::<PointDist>::new();
+    
+    for point in points {
+        distances.push(nth_closest(&tree, &point, 0));
+    }
+
+    let mut circuits = initialise_circuits(tree.len());
+    let mut circuit_map = circuits.iter().enumerate().map(|(i, _)| i).collect::<Vec<usize>>();
     // we now have:
     // indices: a vec of "1" (i.e. the index of the next closest non-self position) for each position in the tree
     // distances: a vec of the index of the point (i.e. the point's i value) together with the distance to its next closest not-yet-added point, and its next closest not-yet-added point
     // circuits: a vec which contains the circuit for each point (as a lookup from point index to a pointer to the set containing it). at the moment this is just {i} for each index i
-    // circuit_map: a map of the point index to the circuit index. this is the map we update when we combine circuits together
+    // circuit_map: a map of the point index to the circuit index. this is the map we update when we combine circuits together. at the moment this is just i for each index i
 
     // the trick is now that we can just iterate on this - we need to only sort the distances array in _descending_ order of distance, then pop off the end
     // this gives us the indices of both points, which we can then merge into one another and increment both indices
@@ -140,32 +150,55 @@ fn part1(tree: KdTree<Point>) {
             panic!("popped two points which didn't refer to one another {first:?} {second:?}");
         }
 
-        // we have our closest pair! now we need to connect them and insert the new points for each
+        // we have our closest pair! now we need to insert the next new points for each, then connect them if we need to
         let i_a = first.i_point_a;
         let i_b = first.i_point_b;
 
-        // arbitrarily merge the second point's circuit into the first's
-        // (aside: this is my first time having to use rc! It makes a lot of
-        // sense actually, compared to languages like Java where I've ended up
-        // making horrid reference cycles by mistake.)
-        // I think there might still be a memory leak here though (or this might be getting cleaned up too early), since I'm not sure when the initial reference goes out of scope...
-        let mut c_a = Rc::clone(&circuits[i_a]);
-        let c_b = Rc::clone(&circuits[i_b]);
+        // we want to fetch the next 
+        indices[i_a] += 1;
+        indices[i_b] += 1;
 
-        // update everything in the second circuit to belong as if it's in the first one, and add each of those values to the first circuit
-        for old_circuit_value in c_b.iter() {
-            c_a.insert(*old_circuit_value);
-            circuits[*old_circuit_value] = Rc::clone(&c_a); 
+        let c_i_a = circuit_map[i_a];
+        let c_i_b = circuit_map[i_b];
+
+        // if the two circuits are the same already, then we've added a redundant connection! no need to merge the circuits
+        if c_i_a == c_i_b {
+            continue;
+        }
+
+        // otherwise, we need to merge the two circuits into one 
+        // arbitrarily merge the second point's circuit into the first's
+        {
+            // we need to clone c_b to iterate over it, because it could in theory be the same as c_a, even though it won't ever be in practice
+            let c_b = circuits[c_i_b].clone();
+            let c_a = &mut circuits[c_i_a];
+
+            // update everything in the second circuit to belong as if it's in the first one, and add each of those values to the first circuit
+            for old_circuit_value in c_b.iter() {
+                c_a.insert(*old_circuit_value);
+                circuit_map[*old_circuit_value] = c_i_a;
+            }
+        }
+
+        // now empty out the other set - we don't want it to count anymore 
+        {
+            let c_b = &mut circuits[c_i_b];
+            c_b.clear();
         }
 
         // if I were writing C this is where I'd free the old circuit
     }
 
+    // we now have some number of sets, and we want the three largest ones
+    // all of the ones which are no longer connected are now empty, so they will have length 0
+    let mut circuit_sizes = circuits.iter().map(|s| s.len()).collect::<Vec<usize>>();
+    circuit_sizes.sort();
+    return circuit_sizes.iter().rev().take(3).product();
 }
 
 impl Puzzle for Day8 {
     fn part1(&self, input: &String) -> String {
-        return input.to_string();
+        return part1(parse_input(input)).to_string(); 
     }
     fn part2(&self, input: &String) -> String {
         return input.chars().rev().collect::<String>();
