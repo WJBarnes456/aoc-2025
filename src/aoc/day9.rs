a1664ac69fbb61acbaa526d7b990d693167ad503
Will Barnes <will@wjbarn.es>
2025-12-09 22:21:35 +0000

    [day9] Implement part 1
    Yeah, very glad I didn't try and do anything clever here, this part 2 is going
    to be tricky enough in itself. I'll give it 15 mins then probably just go to
    bed, I need some sleep.

--- src/aoc/day9.rs
+++ src/aoc/day9.rs
@@ -1,11 +1,55 @@
 use crate::Puzzle;
 
+use itertools::Itertools;
 // Day9 implements day 9 of AoC 2025, as uploaded at https://adventofcode.com/2025/day/9. 
 pub struct Day9;
 
+// after really overcomplicating yesterday I'm going to keep it simple today. Thanks to Wyn for the inspiration on using itertools for this, that combinations function is lush.
+// I'm not even sure what structure would allow you to do a clever thing here - the area between two points is not a metric (exhibit e.g. a triangle, the area between each pair of points is low other than the hypotenuse)
+
+#[derive(Clone, Copy, Debug)]
+struct RedTile {
+    x: usize,
+    y: usize,
+}
+
+impl RedTile {
+    fn area_separating(self: &RedTile, other: &RedTile) -> usize {
+        let (big_x, small_x) = match self.x > other.x {
+            true => (self.x, other.x),
+            false => (other.x, self.x),
+        };
+
+        let (big_y, small_y) = match self.y > other.y {
+            true => (self.y, other.y),
+            false => (other.y, self.y),
+        };
+
+        // we need to add one since the rectangle size is non-zero if they're on the same dimension 
+        let area =  (big_x - small_x + 1) * (big_y - small_y + 1);
+
+        //println!("area between {self:?} and {other:?} is {area}");
+
+        return area;
+    }
+}
+
+fn line_to_redtile(line: &str) -> RedTile {
+    let coords = line.trim().split(',').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>();
+    if coords.len() != 2 {
+        panic!("row should have 2 coords, had {}", coords.len());
+    }
+    return RedTile { x: coords[0], y: coords[1] }
+}
+
 impl Puzzle for Day9 {
     fn part1(&self, input: &String) -> String {
-        return input.to_string();
+        return input.lines()
+                    .map(|l| line_to_redtile(l))
+                    .combinations(2)
+                    .map(|v| v[0].area_separating(&v[1]))
+                    .max().unwrap()
+                    .to_string();
     }
     fn part2(&self, input: &String) -> String {
         return input.chars().take(10).collect::<String>();
